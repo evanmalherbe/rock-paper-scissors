f968ada1fa61e6f546f3d6ddfe7b7114bc81a3d5
Evan Malherbe <evanmalherbe@gmail.com>
2021-09-27 16:35:16 +0200

    Added computerPlay and playRound functions.

--- script.js
+++ script.js
@@ -1,2 +1,52 @@
 // external script file
 
+function computerPlay() {
+    let randomNum = Math.floor(Math.random() * 3) + 1
+    //console.log(randomNum);
+
+    let shakeResult;
+
+    if (randomNum === 1) { shakeResult = "rock"
+    
+    } else if (randomNum === 2) { shakeResult = "paper" }
+    
+    else { shakeResult = "scissors"}
+
+    //console.log(shakeResult);
+
+}
+
+function playRound(playerSelection,computerSelection) {
+    
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        return("It's a tie! Rock against Rock"); 
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return("You lose! Paper beats Rock");
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return("You win! Rock beats Scissors");
+
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+        return("It's a tie! Paper against Paper");
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return("You lose! Scissors beats Paper");
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return("You win! Paper beats Rock");
+
+    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        return("It's a tie! Scissors against Scissors");
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return("You lose! Rock beats Scissors");
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return("You win! Scissors beats Paper");
+    }
+
+}
+
+let playerSelection = "ROck";
+let computerSelection = computerPlay();
+
+console.log(playRound(playerSelection,computerSelection));
+
+
